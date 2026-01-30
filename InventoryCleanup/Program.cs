7d1934524baeb25b00fb6fa91b21ab6c716926ae
saryu <saryum46@gmail,com>
2026-01-30 11:41:57 +0530

    Inventory Cleanup

---
+++ InventoryCleanup/Program.cs
@@ -0,0 +1,29 @@
+﻿using System ; 
+using System.Text ; 
+using System.Globalization ; 
+namespace InventoryCleanup
+{
+  class Program {
+  static void Main(string[] args)
+  {
+    string GivenString ; 
+    GivenString = Console.ReadLine() ; 
+    GivenString = GivenString.Trim() ; 
+    StringBuilder newString =  new StringBuilder() ; 
+    newString.Append(GivenString[0]) ; 
+    for (int i = 1 ; i < GivenString.Length ; i++)
+    {
+       if (GivenString[i] != GivenString[i-1])
+      {
+          newString.Append(GivenString[i]) ; 
+      }
+    } 
+
+    string FinalString = newString.ToString() ; 
+    TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+    string result = textInfo.ToTitleCase(FinalString.ToLower());
+
+    Console.WriteLine(result);
+  }
+  }
+}
