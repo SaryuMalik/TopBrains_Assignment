eb388960c77738441e5bc3911899364474da5f8f
saryu <saryum46@gmail,com>
2026-01-31 10:16:29 +0530

    Commit

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System ;
+using System.Collections.Generic ; 
+
+namespace Dictionary
+{
+  class Program {
+  public static void Main(string[] args)
+  {
+     Dictionary<int , int> EmpDetails = new Dictionary<int , int>()
+     {
+       {1 , 20000} , 
+       {4 , 40000} ,
+       {5 , 15000}
+     }; 
+      
+    List<int> ids = new List<int>() {1 , 4 , 5} ; 
+    int totalSalary = 0 ; 
+
+    foreach (int id in ids)
+    {
+      if (EmpDetails.ContainsKey(id))
+      {
+        totalSalary += EmpDetails[id] ; 
+      }
+    }
+
+    Console.WriteLine(totalSalary) ; 
+
+  }
+  }
+}
